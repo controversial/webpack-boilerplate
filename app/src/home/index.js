f58f8b25b4d1d2fd67dd32f338dccc701518157c
Luke Deen Taylor <luke@deentaylor.com>
2017-02-24 23:34:01 -0500

    Initial commit #2

---
+++ app/src/home/index.js
@@ -0,0 +1,10 @@
+// Styles
+require('./main.sass');
+
+// Libraries
+require('../../../node_modules/ionicons/dist/css/ionicons.css'); // Ionicons
+
+// Files to be copied
+require('./index.pug');
+
+document.body.addElement(document.createTextNode('Hello from JavaScript!'));
